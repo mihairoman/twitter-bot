3fb47e06a5879b12d2b53a07ed8ee5447c313e4d
mihairoman <mihairoman.r@gmail.com>
2017-02-10 00:17:16 +0100

    modified index.js and procfile to fix heroku crash

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 worker: node index.js
-heroku ps:scale worker=0
 heroku ps:scale worker=1
--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-var twitterBot = require('./src/bot');
-twitterBot.init(15, 15);
+var http = require('http'),
+    twitterBot = require('./src/bot'),
+    hostname = '0.0.0.0',
+    port = (process.env.PORT || 3000);
+
+var server = http.createServer(function(req, res) {
+    twitterBot.init(15, 15);
+});
+
+server.listen(port, hostname, function() {
+    console.log(`Server running at http://${hostname}:${port}/ `);
+});
